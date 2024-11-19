052b6a957e0c66c4cbbfa095948aeb71b8a253de
minh2307 <minh23923@gmail.com>
2024-11-19 20:50:44 +0700

    ss2_bai3.cpp

---
+++ ss2_bai3.cpp
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+ 
+ int main(){
+ 	int num1,num2;
+ 	   printf("nhap so thu nhat:");
+ 	   scanf("%d",&num1);
+ 	   printf("nhap so thu hai:");
+ 	   scanf("%d",&num2);
+ 	int tong = num1 + num2;
+ 	   printf("%d + %d = %d\n",num1,num2,tong);
+ 	int hieu = num1 - num2;
+ 	    printf("%d - %d = %d\n",num1,num2,hieu);
+ 	int tich = num1 * num2;
+ 	    printf("%d * %d = %d\n",num1,num2,tich);
+ 	int thuong = num1 / num2;
+ 	    printf("%d / %d = %d\n",num1,num2,thuong);
+ 	
+ 	   
+return 0;
+ }
