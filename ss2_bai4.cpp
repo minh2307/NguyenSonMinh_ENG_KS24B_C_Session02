5d2f8057b28604414af089204877a93075871d36
minh2307 <minh23923@gmail.com>
2024-11-19 20:50:57 +0700

    ss2_bai4.cpp

---
+++ ss2_bai4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int canh;
+        printf("canh cua hinh vuong la:");
+        scanf("%d",&canh);
+    int dien_tich = canh*canh;
+        printf("dien tich hinh vuong la:%d\n",dien_tich);
+    int chu_vi = canh*4;
+        printf("chu vi cua hinh vuong la:%d",chu_vi);
+        
+return 0;
+}
